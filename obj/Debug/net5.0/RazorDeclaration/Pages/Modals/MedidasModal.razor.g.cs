b7adf6be56c74f9455da100c6463485787f8f67d
Derek Palacios <derek2001corez@gmail.com>
2021-12-28 20:44:34 -0600

    adaptacion de codigo para que funcione en mi pc

--- obj/Debug/net5.0/RazorDeclaration/Pages/Modals/MedidasModal.razor.g.cs
+++ obj/Debug/net5.0/RazorDeclaration/Pages/Modals/MedidasModal.razor.g.cs
@@ -13,133 +13,133 @@ namespace TiendaArtesaniasMarielos.Pages.Modals
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\derek\OneDrive\Documentos\Seminario Aletas Michi 2021\AA\AA\AA\_Imports.razor"
+#line 1 "C:\Users\derek\OneDrive\Documentos\Seminario Aletas Michi 2021\AA\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\derek\OneDrive\Documentos\Seminario Aletas Michi 2021\AA\AA\AA\_Imports.razor"
+#line 2 "C:\Users\derek\OneDrive\Documentos\Seminario Aletas Michi 2021\AA\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\derek\OneDrive\Documentos\Seminario Aletas Michi 2021\AA\AA\AA\_Imports.razor"
+#line 3 "C:\Users\derek\OneDrive\Documentos\Seminario Aletas Michi 2021\AA\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\derek\OneDrive\Documentos\Seminario Aletas Michi 2021\AA\AA\AA\_Imports.razor"
+#line 4 "C:\Users\derek\OneDrive\Documentos\Seminario Aletas Michi 2021\AA\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\derek\OneDrive\Documentos\Seminario Aletas Michi 2021\AA\AA\AA\_Imports.razor"
+#line 5 "C:\Users\derek\OneDrive\Documentos\Seminario Aletas Michi 2021\AA\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\derek\OneDrive\Documentos\Seminario Aletas Michi 2021\AA\AA\AA\_Imports.razor"
+#line 6 "C:\Users\derek\OneDrive\Documentos\Seminario Aletas Michi 2021\AA\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\derek\OneDrive\Documentos\Seminario Aletas Michi 2021\AA\AA\AA\_Imports.razor"
+#line 7 "C:\Users\derek\OneDrive\Documentos\Seminario Aletas Michi 2021\AA\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\derek\OneDrive\Documentos\Seminario Aletas Michi 2021\AA\AA\AA\_Imports.razor"
+#line 8 "C:\Users\derek\OneDrive\Documentos\Seminario Aletas Michi 2021\AA\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "C:\Users\derek\OneDrive\Documentos\Seminario Aletas Michi 2021\AA\AA\AA\_Imports.razor"
+#line 11 "C:\Users\derek\OneDrive\Documentos\Seminario Aletas Michi 2021\AA\_Imports.razor"
 using TiendaArtesaniasMarielos;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 12 "C:\Users\derek\OneDrive\Documentos\Seminario Aletas Michi 2021\AA\AA\AA\_Imports.razor"
+#line 12 "C:\Users\derek\OneDrive\Documentos\Seminario Aletas Michi 2021\AA\_Imports.razor"
 using TiendaArtesaniasMarielos.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 13 "C:\Users\derek\OneDrive\Documentos\Seminario Aletas Michi 2021\AA\AA\AA\_Imports.razor"
+#line 13 "C:\Users\derek\OneDrive\Documentos\Seminario Aletas Michi 2021\AA\_Imports.razor"
 using TiendaArtesaniasMarielos.Pages.Modals;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 17 "C:\Users\derek\OneDrive\Documentos\Seminario Aletas Michi 2021\AA\AA\AA\_Imports.razor"
+#line 17 "C:\Users\derek\OneDrive\Documentos\Seminario Aletas Michi 2021\AA\_Imports.razor"
 using TiendaArtesaniasMarielos.Data.Services;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 19 "C:\Users\derek\OneDrive\Documentos\Seminario Aletas Michi 2021\AA\AA\AA\_Imports.razor"
+#line 19 "C:\Users\derek\OneDrive\Documentos\Seminario Aletas Michi 2021\AA\_Imports.razor"
 using TiendaArtesaniasMarielos.Data.Models;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 21 "C:\Users\derek\OneDrive\Documentos\Seminario Aletas Michi 2021\AA\AA\AA\_Imports.razor"
+#line 21 "C:\Users\derek\OneDrive\Documentos\Seminario Aletas Michi 2021\AA\_Imports.razor"
 using TiendaArtesaniasMarielos.Data.Entities;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 26 "C:\Users\derek\OneDrive\Documentos\Seminario Aletas Michi 2021\AA\AA\AA\_Imports.razor"
+#line 26 "C:\Users\derek\OneDrive\Documentos\Seminario Aletas Michi 2021\AA\_Imports.razor"
 using CurrieTechnologies.Razor.SweetAlert2;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 27 "C:\Users\derek\OneDrive\Documentos\Seminario Aletas Michi 2021\AA\AA\AA\_Imports.razor"
+#line 27 "C:\Users\derek\OneDrive\Documentos\Seminario Aletas Michi 2021\AA\_Imports.razor"
 using Sotsera.Blazor.Toaster;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 29 "C:\Users\derek\OneDrive\Documentos\Seminario Aletas Michi 2021\AA\AA\AA\_Imports.razor"
+#line 29 "C:\Users\derek\OneDrive\Documentos\Seminario Aletas Michi 2021\AA\_Imports.razor"
 using BlazorStrap;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 32 "C:\Users\derek\OneDrive\Documentos\Seminario Aletas Michi 2021\AA\AA\AA\_Imports.razor"
+#line 32 "C:\Users\derek\OneDrive\Documentos\Seminario Aletas Michi 2021\AA\_Imports.razor"
 using Radzen;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 33 "C:\Users\derek\OneDrive\Documentos\Seminario Aletas Michi 2021\AA\AA\AA\_Imports.razor"
+#line 33 "C:\Users\derek\OneDrive\Documentos\Seminario Aletas Michi 2021\AA\_Imports.razor"
 using Radzen.Blazor;
 
 #line default
@@ -154,7 +154,7 @@ using Radzen.Blazor;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 35 "C:\Users\derek\OneDrive\Documentos\Seminario Aletas Michi 2021\AA\AA\AA\Pages\Modals\MedidasModal.razor"
+#line 35 "C:\Users\derek\OneDrive\Documentos\Seminario Aletas Michi 2021\AA\Pages\Modals\MedidasModal.razor"
        
 
     BSModal VerticallyCentered;
